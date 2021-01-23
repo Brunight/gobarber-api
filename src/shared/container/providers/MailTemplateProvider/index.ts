ad0af303a800157f61e93ecf2512e9986f96c174
Bruno Rodrigues <brunorodriguesricardo@gmail.com>
2021-01-23 07:15:27 -0300

    Amazon S3 implementation

---
+++ src/shared/container/providers/MailTemplateProvider/index.ts
@@ -0,0 +1,14 @@
+import { container } from 'tsyringe';
+
+import IMailTemplateProvider from './models/IMailTemplateProvider';
+
+import HandlebarsMailTemplateProvider from './implementations/HandlebarsMailTemplateProvider';
+
+const providers = {
+  handlebars: container.resolve(HandlebarsMailTemplateProvider),
+};
+
+container.registerInstance<IMailTemplateProvider>(
+  'MailTemplateProvider',
+  providers.handlebars,
+);
